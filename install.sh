52f3afd5b9c36432e785571856e73a4f4984d95b
jfbueno <buenojeferson@outlook.com>
2025-04-02 17:14:11 -0300

    Cria gitconfig e adiciona symlink no install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ DOTFILES_DIR="$HOME/dotfiles"
 
 ln -sf "$DOTFILES_DIR/rcs/zshrc" "$HOME/.zshrc"
 ln -sf "$DOTFILES_DIR/rcs/bashrc" "$HOME/.bashrc"
+ln -sf "$DOTFILES_DIR/configs/gitconfig" "$HOME/.gitconfig"
 
 ./instal-bat.sh
 ./instal-delta.sh
